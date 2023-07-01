75f57a82ee3fbf8228c7da9552a917309be70d49
jobsontoscano <jjortb@gmail.com>
2023-07-01 17:20:21 -0300

    Primeiras alterações na Home

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>LiveJob</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Empresa de identidades digitais" />
+      <link rel="icon" href="/images/marca/logo-livejob.jpg" />
+    </>
+  );
+}
---
+++ components/Header/menuData.tsx
@@ -0,0 +1,29 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "Quem somos?",
+    path: "/about",
+    newTab: false,
+  },
+  {
+    id: 33,
+    title: "Comunidade",
+    path: "/blog",
+    newTab: false,
+  },
+  {
+    id: 3,
+    title: "Contato",
+    path: "/contact",
+    newTab: false,
+  },
+];
+export default menuData;
